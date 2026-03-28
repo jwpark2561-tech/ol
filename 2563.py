77c6e16ead568e3a87122e2fcc648ac5b30eb77c
jwpark2561-tech <jwpark2561@gmail.com>
2026-03-28 07:11:10 +0000

    3/28

---
+++ 2563.py
@@ -0,0 +1,15 @@
+l=[]
+for i in range(100):
+    l.append([0]*100)
+n=int(input())
+for _ in range(n):
+    a,b=map(int,input().split())
+    for i in range(a,a+10):
+        for j in range(b,b+10):
+            l[i][j]=1
+ans=0
+for i in range(100):
+    for j in range(100):
+        if l[i][j]==1:
+            ans+=1
+print(ans)
