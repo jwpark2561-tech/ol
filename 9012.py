74accfbd7b0b5dd5a9f187fe6252b6a0f6a3644a
jwpark2561-tech <jwpark2561@gmail.com>
2026-04-19 03:39:06 +0000

    결과 변수 초기화 및 출력 위치 수정

--- 9012.py
+++ 9012.py
@@ -1,16 +1,22 @@
 n=int(input())
 l=[]
+result = True
 for _ in range(n):
     s=input()
     for i in s:
         if i=='(':
             l.append(i)
+            print(l)
         elif i==')':
             if len(l)==0:
-                print('NO')
+                result = False
                 break
             l.pop()
-    if len(l)==0:
-        print('YES')
-    else:
+            print(l)
+    if result == False:
         print('NO')
+    else:
+        if len(l)==0:
+            print('YES')
+        else:
+            print('NO')
